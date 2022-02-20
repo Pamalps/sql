b666a04713a8a915d36475ea779e7e23daed54f6
pamalps <pamalpreet@gmail.com>
2022-02-19 20:14:10 -0500

    mc

--- useful_code.sql
+++ useful_code.sql
@@ -2,3 +2,14 @@
 
 -- trim function
 select trim(col_name) from table_name;
+
+-- convert a value to integer
+select cast(col_name as int);
+
+-- case statement for fillin nulls
+select
+case when col_name IS NULL 
+    then 'P'
+    else col_name
+    end as col_name
+from table_name
